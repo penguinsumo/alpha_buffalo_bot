0198d733993a0ab86dac363da5586600f4c265fa
penguinsumo <penguinsumo@gmail.com>
2026-06-24 16:30:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'status': 'OK', 'bot': 'Alpha Buffalo v11.2'}
+# ใช้ @app.api_route เพื่อให้รองรับทั้ง GET, HEAD, POST ฯลฯ
+@app.api_route("/", methods=["GET", "HEAD", "POST"])
+async def root(request: Request):
+    return {"status": "OK", "bot": "Alpha Buffalo v11.2"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
